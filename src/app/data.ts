51daa028c657575823bcd5d0f0f74d35af5be495
Richard Rissanen <richardrissanen@me.com>
2017-10-03 17:51:44 -0500

    pulled events data out to src/app/data.ts

--- src/app/data.ts
+++ src/app/data.ts
@@ -3,8 +3,8 @@ export var data = [
         id: 1,
         title: 'Event Title',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        startTime: 'Thu Jan 03 1985 11:00:00 GMT-0400',
-        endTime: 'Thu Jan 03 1985 11:45:00 GMT-0400',
+        startTime: new Date('Thu Jan 03 1985 11:00:00 GMT-0400'),
+        endTime: new Date('Thu Jan 03 1985 11:45:00 GMT-0400'),
         location: 'Location #1',
         host: 'Hostname',
         feature: 'Feature Name'
@@ -13,8 +13,8 @@ export var data = [
         id: 2,
         title: 'Event Title #2',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        startTime: 'Thu Jan 03 1985 11:00:00 GMT-0400',
-        endTime: 'Thu Jan 03 1985 11:45:00 GMT-0400',
+        startTime: new Date('Thu Jan 03 1985 11:00:00 GMT-0400'),
+        endTime: new Date('Thu Jan 03 1985 11:45:00 GMT-0400'),
         location: 'Location #2',
         host: 'Hostname',
         feature: 'Feature Name'
@@ -23,8 +23,8 @@ export var data = [
         id: 3,
         title: 'Event Title #3',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        startTime: 'Thu Jan 03 1985 11:45:00 GMT-0400',
-        endTime: 'Thu Jan 03 1985 12:45:00 GMT-0400',
+        startTime: new Date('Thu Jan 03 1985 11:45:00 GMT-0400'),
+        endTime: new Date('Thu Jan 03 1985 12:45:00 GMT-0400'),
         location: 'Location #1',
         host: 'Hostname',
         feature: 'Feature Name'
@@ -33,8 +33,8 @@ export var data = [
         id: 4,
         title: 'Event Title #4',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        startTime: 'Thu Jan 03 1985 11:45:00 GMT-0400',
-        endTime: 'Thu Jan 03 1985 12:45:00 GMT-0400',
+        startTime: new Date('Thu Jan 03 1985 11:45:00 GMT-0400'),
+        endTime: new Date('Thu Jan 03 1985 12:45:00 GMT-0400'),
         location: 'Location #2',
         host: 'Hostname',
         feature: 'Feature Name'
