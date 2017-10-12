49151c1fb3a3e8e1ad5f8694ad4b2cd7af6dc542
Richard Rissanen <richardrissanen@me.com>
2017-10-12 15:25:48 -0500

    Added Safari launcher because I uninstalled Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -8,9 +8,10 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-safari-launcher'),      
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -27,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Safari'],
     singleRun: false
   });
 };
