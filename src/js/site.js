393b55768ce05b56ce086a399a7c2fff65761e30
Richard Rissanen <richardrissanen@me.com>
2018-02-02 22:11:00 -0600

    Added data, init local storage, and toggling of stars.

--- src/js/site.js
+++ src/js/site.js
@@ -7,7 +7,9 @@ filterEvents = function() {
   posts = document.getElementsByClassName('list-group-item');
 
   if (query.length > 0) {
+
     results = [];
+
     for (i = 0, len = posts.length; i < len; i++) {
       post = posts[i];
       results.push((function(post) {
@@ -18,18 +20,90 @@ filterEvents = function() {
         }
       })(post));
     }
+
     return results;
 
   } else {
-    results1 = [];
+
+    results2 = [];
+
     for (j = 0, len1 = posts.length; j < len1; j++) {
       post = posts[j];
-      results1.push((function(post) {
+      results2.push((function(post) {
         return post.style.display = 'block';
       })(post));
     }
-    return results1;
+
+    return results2;
   }
-};
+}
 
+////
+// Local Storage
+////
+var favoriteToggles = document.getElementsByClassName("favorite-toggle");
 
+Array.prototype.forEach.call(favoriteToggles, function(toggle, i){
+
+  toggle.addEventListener("click", function(event){
+    event.preventDefault();
+
+    this.classList.toggle('empty');
+
+    var eventId = this.getAttribute('data-id');
+    var favoritesArray = new Array;
+
+    if (typeof (Storage) !== "undefined") {
+      var favorites = localStorage.getItem('favorites');
+
+      if (favorites !== null && typeof favorites !== 'undefined') {
+        favoritesArray = JSON.parse(favorites);
+
+        if (favoritesArray.indexOf(eventId) === -1) {
+          favoritesArray.push(eventId);
+        }
+        else {
+          favoritesArray = favoritesArray.filter(function (favoriteId) { return favoriteId !== eventId; });
+        }
+
+      }
+      else {
+        favoritesArray = [];
+        favoritesArray.push(eventId);
+      }
+
+      var favoritesArrayString = JSON.stringify(favoritesArray);
+      localStorage.setItem('favorites', favoritesArrayString);
+    }
+    else {
+      alert('Sorry, your web browser does not support local storage.');
+    }
+
+    console.log("favoritesArrayString ==" + favoritesArrayString);
+
+    return true;
+
+  });
+
+});
+
+document.getElementById("favorite").addEventListener("click", function(event){
+  event.preventDefault();
+
+  this.classList.toggle('empty');
+
+});
+
+fetchFavorites = function()
+{
+  var favorites = localStorage.getItem('favorites');
+  var favoritesArray;
+
+  if (favorites !== null && typeof favorites !== 'undefined') {
+      favoritesArray = JSON.parse(favorites);
+  }
+  else {
+      favoritesArray = [];
+  }
+  return favoritesArray;
+}
