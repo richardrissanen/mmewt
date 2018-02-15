2dd6cf6d2f35f389e40673c75a975b3c193b3b0c
Richard Rissanen <richard@meetyourdeveloper.com>
2018-02-15 14:28:02 -0600

    Create template_module.js

---
+++ js/modules/template_module.js
@@ -0,0 +1,53 @@
+define(["./date_module"], function(dateModule) {
+
+    var dateFormat = new dateFormatModule();
+
+    function checkIfInPast(start) {
+        var classToAdd = "current";
+
+        if (start < new Date())
+            classToAdd = 'past-event';
+
+        return classToAdd;
+    }
+
+    var templateModule = function() {
+  
+        this.createEventHtml = function(title, start, id, description) {
+
+            var classToAdd = checkIfInPast(start);
+            var transformedDate = dateFormat.transform(start);
+
+            return  `<li id="event${id}"class="list-group-item event-list ${classToAdd} hide">
+                        <h6>
+                            ${title} <small class="text-muted">${transformedDate}</small>
+                            <a href="#" class="favorite-toggle star empty" data-id="${id}"></a>
+                        </h6>
+                        <p>${description}</p>
+                    </li>`;
+        }
+
+        this.createEmptyState = function() {
+            var emptyHtml = `<div id="empty-state-container" class="text-center ml-auto mr-auto">
+                                <h6>Oh no, you have no favorites.</h6>
+                                <div class="big-star mt-4 mb-4"></div>
+                                <p>Go to Events and add favorites by tapping the star on the right.</p>
+                            </div>`;
+            return `<li>${emptyHtml}<li>`;
+        }
+
+        this.createNoLocalStorageState = function() {
+            var emptyHtml = `<div id="empty-state-container" class="text-center ml-auto mr-auto">
+                                <h6>Oh no, your browser does not have local storage.</h6>
+                                <p> We don&#39;t want to annoy users with creating an account. As a result, we require local storage to run this app. We apologize for the inconvenience.</p>
+                            </div>`;
+            return `<li>${emptyHtml}<li>`;
+        }
+
+  
+    };
+  
+    return templateModule;
+  
+  });
+  
