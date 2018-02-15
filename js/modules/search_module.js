44025b96695c394dab2a36548630c5f502738f28
Richard Rissanen <richard@meetyourdeveloper.com>
2018-02-15 14:27:27 -0600

    Update search_module.js

--- js/modules/search_module.js
+++ js/modules/search_module.js
@@ -1,14 +1,39 @@
 define([], function() {
 
-  var scrollModule = function() {
+  function transfromTime(hours, minutes) {
+    var suffix = "am";
 
-    this.toTop = function() {
-      if (window.scrollY !== 0)
-        window.scroll(0,0);
+    if (hours == 0) {
+      hours = 12;
+    } else {
+      if (hours > 12) {
+        suffix = "pm";
+        hours = hours - 12;
+      } else {
+        hours = hours;
+      }
+    }
+
+    hours = checkForUnderTen(hours)
+    minutes = checkForUnderTen(minutes);
+
+    return hours + ":" + minutes + suffix;
+  }
+
+  function checkForUnderTen(dateFragment) {
+    if (dateFragment < 10) 
+      dateFragment = "0" + dateFragment;
+
+    return dateFragment;
+  }
+
+  var dateFormatModule = function() {
+    this.transform = function(date) {
+      var month = date.getMonth() + 1;
+      return date.getFullYear() + '/' + checkFoUnderTen(month) + '/' + checkForUnderTen(date.getDate()) + " at " + transfromTime(date.getHours(), date.getMinutes());
     };
+  }
 
-  };
-
-  return scrollModule;
+  return dateFormatModule;
 
 });
